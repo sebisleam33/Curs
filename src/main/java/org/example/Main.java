714795b8e9e75c4b635169902e52c2c221fb9d99
kennyder <sebastian.sleam01@e-uvt.ro>
2024-05-25 14:19:49 +0300

    Updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,42 @@
+/*
+ * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package org.example;
 
 import java.util.ArrayList;
 
+class IntWrapper {
+    public int num;
+
+    public IntWrapper(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> listNames = new ArrayList<>();
-        listNames.add("stefan");
-        listNames.add("sebi");
-        listNames.add("andrei");
-        listNames.add("raul");
-        listNames.add("ema");
-        System.out.println(listNames);
-//        for (String temp: listNames) {
-//            System.out.println(temp);
-//        }
+        ArrayList<Integer> numbersList = new ArrayList<>();
+        numbersList.add(21);
+        System.out.println(numbersList.get(0));
 
-        System.out.println(listNames.get(2));
-        System.out.println(listNames.remove("raul"));
-        listNames.remove("raul");
-        System.out.println(listNames);
-        System.out.println(listNames.remove("john"));
-        System.out.println(listNames.set(0, "alex"));
-        System.out.println(listNames);
-        System.out.println(listNames.indexOf("sebi"));
-        System.out.println(listNames.indexOf("bogdan"));
+        ArrayList<IntWrapper> numbersList2 = new ArrayList<>();
+        numbersList2.add(new IntWrapper(55));
+        System.out.println(numbersList2.get(0).getNum());
 
     }
 }
