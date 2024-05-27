b7f635d870dcfdb77c76a0315a1cd1a4a2e56544
kennyder <sebastian.sleam01@e-uvt.ro>
2024-05-27 15:40:38 +0300

    Updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,52 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+class Data {
+    private Object object;
+
+    public Data(Object object) {
+        this.object = object;
+    }
+
+    @Override
+    public String toString() {
+        return object.toString();
+    }
+
+    public Object getObject() {
+        return object;
+    }
+}
+class Name {
+   private String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList<String> names = new LinkedList<>();
-        names.add("sebi");
-        names.add("andre");
-        names.add("claudu");
-        names.add("debi");
-        names.add("raul");
+        List<Data> elements = new LinkedList<>();
+        elements.add(new Data("Sebi"));
+        elements.add(new Data("Andrei"));
+        elements.add(new Data("Oradea"));
+        elements.add(new Data("Cluj"));
+        elements.add(new Data(2));
+        elements.add(new Data(new Name("Sleam")));
+        System.out.println(elements);
 
-        ArrayList<String> list2 = new ArrayList<>();
-        list2.add("Dobrogea");
-        list2.add("Baia Mare");
-
-        new Main().printList(names);
-        new Main().printList(list2);
-
+        Data x = new Data(new Name("Amalia"));
+        System.out.println(x.getObject());
     }
-    
-    void printList(List<String> list){
-        for (String name : list) {
-            System.out.println(name);
-        }
-    }
+
+
+
 }
