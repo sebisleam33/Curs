58b01ceb71eee38d01066eb4d0c163b2468fc6a9
kennyder <sebastian.sleam01@e-uvt.ro>
2024-05-27 16:20:53 +0300

    Updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,28 +3,41 @@ package org.example;
 import java.util.LinkedList;
 import java.util.List;
 
-class Data <T> {
-    private T object;
+class Data <K, V> {
+   private K key;
+   private V value;
 
-    public Data(T object) {
-        this.object = object;
+    public Data(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
     }
 
     @Override
     public String toString() {
-        return object.toString();
+        return "Data{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+
+    public <E, N> void display(E element, N number) {
+        System.out.println("Element: "+element+" Number: "+number);
     }
 }
 public class Main {
     public static void main(String[] args) {
 
-        List<Data<String>> elements = new LinkedList<>();
-        elements.add(new Data<>("Sebi"));
-        elements.add(new Data<>("Amalia"));
-
-        for (Data<String> data: elements) {
-            System.out.println(data);
-        }
+        Data<Integer, String> data = new Data<>(10, "Sebi");
+        System.out.println(data);
+        data.display("Sebastianul", 22500);
 
     }
 }
