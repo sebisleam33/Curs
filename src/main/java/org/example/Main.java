c8da6da0afd38c3c5b5e02f94e903e91acdd61c3
kennyder <sebastian.sleam01@e-uvt.ro>
2024-05-29 16:20:29 +0300

    Updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,56 +1,27 @@
 package org.example;
 
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-class Data <K extends Integer, V extends String> {
-   private K key;
-   private V value;
-
-    public Data(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    @Override
-    public String toString() {
-        return "Data{" +
-                "key=" + key +
-                ", value=" + value +
-                '}';
-    }
-
-}
 public class Main {
     public static void main(String[] args) {
-        Comparator<Data<Integer, String>> COMPARE_KEY = new Comparator<Data<Integer, String>>() {
-            @Override
-            public int compare(Data<Integer, String> obj1, Data<Integer, String> obj2) {
-                if(obj1.getKey() < obj2.getKey()){
-                    return -1;
-                }else if (obj1.getKey() > obj2.getKey()){
-                    return 1;
-                }
-                return 0;
-            }
-        };
-        Set<Data<Integer, String>> set = new TreeSet<>(COMPARE_KEY);
-        set.add(new Data<>(1, "sebi"));
-        set.add(new Data<>(2, "raul"));
-        set.add(new Data<>(3, "amalia"));
-        set.add(new Data<>(4, "patric"));
-        set.add(new Data<>(5, "alex"));
-        set.add(new Data<>(1, "sebi"));
-
-        for (Data data: set) {
-            System.out.println(data);
+        Queue<Integer> queue = new ArrayBlockingQueue<>(4);
+        try {
+            queue.offer(1);
+            queue.offer(2);
+            queue.offer(3);
+            queue.offer(4);
+            queue.offer(5);
+            System.out.println(queue.offer(6));
+            System.out.println(queue.poll());
+            queue.offer(6);
+        } catch (Exception e) {
+            System.out.println("Wrong");
         }
+
+        System.out.println(queue.peek());
+        System.out.println(queue.element());
+        System.out.println(queue);
     }
 }
