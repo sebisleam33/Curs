c0c44aa6aa39a0e2b709c3c1700390e54fd75fe7
kennyder <sebastian.sleam01@e-uvt.ro>
2024-06-02 13:18:15 +0300

    Updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,33 @@
 package org.example;
-import java.util.*;
-import static java.lang.Thread.sleep;
+
+class Brackets {
+    synchronized public void generate() {
+        for (int i = 1; i <= 20 ; i++) {
+            if(i<=10){
+                System.out.print('[');
+            } else {
+                System.out.print(']');
+            }
+        }
+        System.out.println();
+    }
+}
 public class Main {
     public static void main(String[] args) {
+        Brackets brackets = new Brackets();
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 10; i++) {
-                    try {
-                        Random random = new Random();
-                        sleep(random.nextInt(1000));
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println(i);
+                for (int i = 0; i < 5; i++) {
+                    brackets.generate();
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    brackets.generate();
                 }
             }
         }).start();
