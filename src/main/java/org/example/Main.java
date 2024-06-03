dbad7ffbe58a0367b198e7c94529a2fd3a6192cd
kennyder <sebastian.sleam01@e-uvt.ro>
2024-06-03 14:22:02 +0300

    Updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,65 +2,46 @@ package org.example;
 
 
 public class Main {
-    public static int balance = 0;
-    synchronized public void withdrawn(int amount) {
-        if(balance<=0) {
-            System.out.println("Waiting for amount to be updated.");
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                System.out.println("Withdrawal interrupted");
-            }
-        }
-        if((balance-amount)<0){
-            System.out.println("Balance too low for withdrawal: "+balance);
-            System.out.println("Withdrawal incomplete");
-            return;
-        }
-        balance = balance - amount;
-        System.out.println("The current balance is: "+balance);
-    }
-
-    public boolean deposit(int amount) {
-        synchronized (this) {
-            if (amount > 0) {
-                System.out.println("We are depositing the amount in the bank: " + amount);
-                balance = balance + amount;
-                notify();
-                return true;
-            } else {
-                System.out.println("Invalid amount");
-                return false;
-            }
-        }
-    }
-    public static void main(String[] args) {
-        Main main = new Main();
+    public static int counter1= 0;
+    public static int counter2= 0;
+    public static void main(String[] args) throws InterruptedException {
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                main.withdrawn(1000);
+                for (int i = 0; i < 1000; i++) {
+                    counter1++;
+                }
             }
         });
-        thread1.setName("Thread 1");
         thread1.start();
-
         Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                if(main.deposit(2000)){
-                    System.out.println("Transaction completed");
-                } else {
-                    thread1.interrupt();
+                for (int i = 0; i < 1000; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    counter2++;
                 }
             }
         });
-        thread2.setName("Thread 2");
         thread2.start();
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    thread1.join();
+                    thread2.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("c1 "+counter1);
+                System.out.println("c2 "+counter2);
+            }
+        });
+        thread3.start();
+        System.out.println("Main thread: "+Thread.currentThread().getName());
     }
 }
