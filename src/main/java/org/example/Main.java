bb54e5c3dd7b108e1e03ff2773d023c2fa7c20f4
kennyder <sebastian.sleam01@e-uvt.ro>
2024-06-03 14:40:57 +0300

    Updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,37 @@
 package org.example;
 
+import java.util.concurrent.CountDownLatch;
+
+class someThread extends Thread {
+    private CountDownLatch latch;
+
+    public someThread(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+
+    public void run () {
+        System.out.println("Started thread: "+Thread.currentThread().getName());
+        System.out.println("Ended thread: " + Thread.currentThread().getName());
+        System.out.println("**************");
+        latch.countDown();
+    }
+}
 
 public class Main {
-    public static int counter1= 0;
-    public static int counter2= 0;
     public static void main(String[] args) throws InterruptedException {
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 1000; i++) {
-                    counter1++;
-                }
-            }
-        });
+        CountDownLatch latch = new CountDownLatch(4);
+        someThread thread1 = new someThread(latch);
+        someThread thread2 = new someThread(latch);
+        someThread thread3 = new someThread(latch);
+        someThread thread4 = new someThread(latch);
         thread1.start();
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 1000; i++) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    counter2++;
-                }
-            }
-        });
         thread2.start();
-        Thread thread3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    thread1.join();
-                    thread2.join();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("c1 "+counter1);
-                System.out.println("c2 "+counter2);
-            }
-        });
         thread3.start();
-        System.out.println("Main thread: "+Thread.currentThread().getName());
+        thread4.start();
+
+        latch.await();
+        System.out.println("Main thread: "+ Thread.currentThread().getName());
     }
 }
