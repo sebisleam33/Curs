b6540dd947b6b5d2790c705cd7b320174fe295f3
kennyder <sebastian.sleam01@e-uvt.ro>
2024-06-03 13:54:27 +0300

    Updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,51 @@
 package org.example;
 
-class Brackets {
-    synchronized public void generate() {
-        for (int i = 1; i <= 20 ; i++) {
-            if(i<=10){
-                System.out.print('[');
-            } else {
-                System.out.print(']');
-            }
-        }
-        System.out.println();
-    }
-}
+
 public class Main {
+    public static int balance = 0;
+    synchronized public void withdrawn(int amount) throws InterruptedException {
+        if(balance<=0) {
+            System.out.println("Waiting for amount to be updated.");
+            wait();
+        }
+        balance = balance - amount;
+        System.out.println("The current balance is: "+balance);
+    }
+
+    public void deposit(int amount) {
+        synchronized (this){
+            System.out.println("We are depositing the amount in the bank: " + amount);
+            balance = balance + amount;
+            notify();
+        }
+    }
     public static void main(String[] args) {
-        Brackets brackets = new Brackets();
-        new Thread(new Runnable() {
+        Main main = new Main();
+        Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 5; i++) {
-                    brackets.generate();
+                try {
+                    main.withdrawn(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
-        }).start();
-        new Thread(new Runnable() {
+        });
+        thread1.setName("Thread 1");
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 5; i++) {
-                    brackets.generate();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
+                main.deposit(2000);
             }
-        }).start();
+        });
+        thread2.setName("Thread 2");
+        thread2.start();
     }
 }
