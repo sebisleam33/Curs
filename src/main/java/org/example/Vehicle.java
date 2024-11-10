21cabf8c21f07fbd96bddf261e040d317f335a8c
kennyder <sebastian.sleam01@e-uvt.ro>
2024-11-10 11:49:03 +0200

    Updates

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -6,20 +6,17 @@ public class Vehicle implements Serializable {
 
     private final String type;
     private final int number;
-    private final int kilometers;
 
     public Vehicle(String type, int number) {
         this.type = type;
         this.number = number;
-        this.kilometers = kilometers;
     }
 
     @Override
     public String toString() {
         return "Vehicle{" +
                 "type='" + type + '\'' +
-                ", number=" + number + '\'' +
-                ", kilometers =" + kilometers
+                ", number=" + number +
                 '}';
     }
 }
